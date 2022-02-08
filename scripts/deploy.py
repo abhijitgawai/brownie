ae9cf0fce93a86f835d113064230f8c75714f3e2
abhijitgawai <abhijit.gawai18@vit.edu>
2022-02-08 17:13:06 +0530

    deployed SimpleStorage from account

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,10 +1,19 @@
-from brownie import accounts
+from brownie import accounts, SimpleStorage
 
 def deploy_simple_storage():
-    # account = accounts[0]
+    account = accounts[0]
     # print(account)
-    account = accounts.load("abhijit_account")
-    print(account)
+    # account = accounts.load("abhijit_account")
+    # print(account)
+    
+    # There are many ways to keep private key in .env etc..learn at https://youtu.be/M576WGiDBdQ?t=16796
+
+    simple_storage = SimpleStorage.deploy({"from":account})
+    # Transaction            // brownie smart enough to understand this is transaction
+    # Call
+    print(simple_storage    )
+
+
 
 def main():
     deploy_simple_storage()
